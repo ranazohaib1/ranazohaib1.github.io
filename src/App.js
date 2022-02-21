0d9d47c40b05724f92a9205cb5453a5a0f1e63ce
Zohaib <zohaib.dilshad.1@gmail.com>
2022-02-21 11:50:20 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Home from './components/home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+ 
+    </div>
+  );
+}
+
+export default App;
